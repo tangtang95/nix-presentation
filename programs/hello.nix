a9a7c542963ad91a622a6620686d61643a60eec6
Tangtang Zhou <tangtang2995@gmail.com>
2026-04-29 11:30:17 +0200

    chore: refactor hello

--- programs/hello.nix
+++ programs/hello.nix
@@ -1,10 +1,10 @@
 { pkgs }:
 let
-  world = "world";
+  world = { foo = "world"; };
   y = x + 1;
   x = 3;
 in
 pkgs.writeShellScriptBin "hello" ''
-  ${pkgs.cowsay}/bin/cowsay "hello ${world} ${toString (y * 2)}!"
+  ${pkgs.cowsay}/bin/cowsay "hello ${world.foo} ${toString (y * 2)}!"
 ''
 
